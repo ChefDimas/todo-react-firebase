a8a90732c15dbe0771fc31dd444368d501e728fd
ChefDimas <dimalunik@aol.com>
2022-11-24 21:38:57 +0100

    Add counter at bottom

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
 						<Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
 					))}
 				</ul>
-				<p className={style.count}>You have 2 todos</p>
+				<p className={style.count}>You have {todos.length}</p>
 			</div>
 		</div>
 	);
